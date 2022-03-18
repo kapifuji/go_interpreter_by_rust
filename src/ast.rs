567fd52da633148d506e077a8999de69a834fa83
sk <dummy>
2022-03-18 11:38:19 +0900

    Title: ソースコード復元機能を実装

--- src/ast.rs
+++ src/ast.rs
@@ -24,8 +24,71 @@ impl Program {
             statements: Vec::new(),
         }
     }
+    pub fn to_code(&self) -> String{
+        let mut code = String::new();
+        for statement in &self.statements{
+            code.push_str(statement.to_code().as_str());
+            code.push('\n');
+        }
+        code
+    }
 }
 
-impl Statement {}
+impl Statement {
+    pub fn to_code(&self) -> String{
+        let mut statement = String::new();
+        match self{
+            Statement::Let{identifier, value} => {
+                statement.push_str("let ");
+                statement.push_str(identifier.to_code().as_str());
+                statement.push_str(" = ");
+                statement.push_str(value.to_code().as_str());
+                statement.push(';');
+            },
+            Statement::Return(expression) => {
+                statement.push_str("return ");
+                statement.push_str(expression.to_code().as_str());
+                statement.push(';');
+            }
+        }
+        statement
+    }
+}
 
-impl Expression {}
+impl Expression {
+    pub fn to_code(&self) -> String{
+        match self{
+            Expression::Identifier(identifier) => identifier.to_string(),
+            Expression::Integer(integer) => integer.to_string(),
+            Expression::Illegal => "[illegal expression]".to_string()
+        }
+    }
+}
+
+
+#[cfg(test)]
+mod tests{
+    use super::*;
+
+    #[test]
+    fn test_to_code() {
+        let expected_code = "let x = 100;
+return x;
+";
+        let mut program = Program::new();
+
+        let statement1 = Statement::Let{
+            identifier: Expression::Identifier("x".to_string()),
+            value: Expression::Integer(100)
+        };
+
+        let statement2 = Statement::Return(
+            Expression::Identifier("x".to_string())
+        );
+
+        program.statements.push(statement1);
+        program.statements.push(statement2);
+
+        assert_eq!(program.to_code(), expected_code);
+    }
+}
