9d856396751952375389599ec11a47a17fb2a3b3
sk <dummy>
2022-04-05 15:47:10 +0900

    中置演算子（+, -, *, /）の評価器を実装

--- src/evaluator.rs
+++ src/evaluator.rs
@@ -36,6 +36,15 @@ impl Evaluator {
                 let object = Evaluator::eval_expression(expression);
                 Evaluator::eval_prefix_expression(operator.clone(), &(object?))
             }
+            ast::Expression::InfixExpression {
+                left,
+                operator,
+                right,
+            } => {
+                let left = Evaluator::eval_expression(left)?;
+                let right = Evaluator::eval_expression(right)?;
+                Evaluator::eval_infix_expression(&left, operator.clone(), &right)
+            }
             _ => Ok(object::Object::Null),
         }
     }
@@ -46,7 +55,33 @@ impl Evaluator {
     ) -> Result<object::Object, Box<dyn std::error::Error>> {
         match operator {
             operator::Prefix::Exclamation => Evaluator::eval_exclamation_operator(object),
-            operator::Prefix::Minus => Evaluator::eval_minus_operator(object),
+            operator::Prefix::Minus => Evaluator::eval_minus_prefix_operator(object),
+            _ => Ok(object::Object::Null),
+        }
+    }
+
+    fn eval_infix_expression(
+        left: &object::Object,
+        operator: operator::Infix,
+        right: &object::Object,
+    ) -> Result<object::Object, Box<dyn std::error::Error>> {
+        let left = if let object::Object::Integer(integer) = left {
+            integer
+        } else {
+            return Ok(object::Object::Null);
+        };
+
+        let right = if let object::Object::Integer(integer) = right {
+            integer
+        } else {
+            return Ok(object::Object::Null);
+        };
+
+        match operator {
+            operator::Infix::Plus => Ok(object::Object::Integer(left + right)),
+            operator::Infix::Minus => Ok(object::Object::Integer(left - right)),
+            operator::Infix::Asterisk => Ok(object::Object::Integer(left * right)),
+            operator::Infix::Slash => Ok(object::Object::Integer(left / right)),
             _ => Ok(object::Object::Null),
         }
     }
@@ -61,7 +96,7 @@ impl Evaluator {
         }
     }
 
-    fn eval_minus_operator(
+    fn eval_minus_prefix_operator(
         object: &object::Object,
     ) -> Result<object::Object, Box<dyn std::error::Error>> {
         match object {
@@ -79,7 +114,19 @@ mod tests {
 
     #[test]
     fn test_eval_integer_expression() {
-        let tests = [("1", 1), ("12", 12), ("-1", -1), ("-12", -12)];
+        let tests = [
+            ("1", 1),
+            ("12", 12),
+            ("-1", -1),
+            ("-12", -12),
+            ("1 + 2 - 3", 0),
+            ("1 + 2 * 3", 7),
+            ("3 * 4 / 2 + 10 - 8", 8),
+            ("(1 + 2) * 3 - -1", 10),
+            ("-1 * -1", 1),
+            ("-10 + -1 * 2", -12),
+            ("(10 + 20) / (10 - 0)", 3),
+        ];
 
         for (input, result) in tests {
             let evaluated = test_eval(input);
